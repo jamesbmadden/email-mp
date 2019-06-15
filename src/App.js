16629a17f021e7dfcb84ce13b18ab00c312bbd5c
James Madden <jamesbmadden@gmail.com>
2019-06-15 16:17:30 -0700

    Track Postal Code Value & New Icon

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './icons/icon.svg';
 import search from './icons/search.svg';
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <span>Email <span className="title-bold">Members of Parliament</span></span>
-      </header>
-      <main className="App-main">
-        <h1 className="title-reg">Enter Your <span className="title-bold">Postal Code</span>:</h1>
-        <form className="search-bar">
-          <input className="App-postal title-bold" placeholder="a1b2c3"></input>
-          <button><img src={search} alt="->" /></button>
-        </form>
-      </main>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value.toUpperCase()});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <span>Email <span className="title-bold">MPs</span></span>
+        </header>
+        <main className="App-main">
+          <h1 className="title-reg">Enter Your <span className="title-bold">Postal Code</span>:</h1>
+          <form className="search-bar" onSubmit={this.handleSubmit}>
+            <input className="App-postal title-bold" name="postal" id="postal" placeholder="a1b2c3" type="text" 
+            value={this.state.value} onChange={this.handleChange} maxLength="6"></input>
+            <button><img src={search} alt="->" /></button>
+          </form>
+        </main>
+      </div>
+    );
+  }
+
 }
 
 export default App;
