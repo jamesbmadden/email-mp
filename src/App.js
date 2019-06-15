3634c6dd0276833cee403adcce15543d08133877
James Madden <jamesbmadden@gmail.com>
2019-06-15 15:34:16 -0700

    Basic App Homescreen Layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import search from './icons/search.svg';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <span>Email <span className="title-bold">Members of Parliament</span></span>
       </header>
+      <main className="App-main">
+        <h1 className="title-reg">Enter Your <span className="title-bold">Postal Code</span>:</h1>
+        <form className="search-bar">
+          <input className="App-postal title-bold" placeholder="a1b2c3"></input>
+          <button><img src={search} alt="->" /></button>
+        </form>
+      </main>
     </div>
   );
 }
