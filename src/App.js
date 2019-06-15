44bb6285fedd8cc54e04a84839c873c66c99a764
James Madden <jamesbmadden@gmail.com>
2019-06-15 16:32:50 -0700

    Add logic for showing loading screen

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {value: ''};
+    this.state = {value: '', loading: false};
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -18,11 +18,23 @@ class App extends React.Component {
   }
 
   handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
+    this.setState({loading: true});
     event.preventDefault();
   }
 
   render() {
+
+    let loading = this.state.loading && (<>
+      <p>Loading...</p>
+    </>)
+
+    let forum = !this.state.loading && (<><h1 className="title-reg">Enter Your <span className="title-bold">Postal Code</span>:</h1>
+      <form className="search-bar" onSubmit={this.handleSubmit}>
+        <input className="App-postal title-bold" name="postal" id="postal" placeholder="A1B2C3" type="text" 
+        value={this.state.value} onChange={this.handleChange} maxLength="6"></input>
+        <button><img src={search} alt="->" /></button>
+      </form></>)
+
     return (
       <div className="App">
         <header className="App-header">
@@ -30,12 +42,8 @@ class App extends React.Component {
           <span>Email <span className="title-bold">MPs</span></span>
         </header>
         <main className="App-main">
-          <h1 className="title-reg">Enter Your <span className="title-bold">Postal Code</span>:</h1>
-          <form className="search-bar" onSubmit={this.handleSubmit}>
-            <input className="App-postal title-bold" name="postal" id="postal" placeholder="a1b2c3" type="text" 
-            value={this.state.value} onChange={this.handleChange} maxLength="6"></input>
-            <button><img src={search} alt="->" /></button>
-          </form>
+          {forum}
+          {loading}
         </main>
       </div>
     );
