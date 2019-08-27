a4b9581e4bb7880d585b9c9a294bba25058c6d9b
James Madden <jamesbmadden@gmail.com>
2019-08-27 11:29:53 -0700

    Sort by MP, then MLA, then city reps

--- src/components/MP.js
+++ src/components/MP.js
@@ -6,14 +6,15 @@ export default class MP extends React.Component {
   constructor(props) {
     super(props);
     this.postal = props.postal;
-    this.state = {mp: undefined, reps: undefined};
+    this.state = {mp: undefined, reps: undefined, mla: undefined};
     console.log(`https://represent.opennorth.ca/postcodes/${this.postal}/`);
     fetch(`https://represent.opennorth.ca/postcodes/${this.postal}/`)
       .then(response => response.json())
       .then(riding => {
         let mpList = riding.representatives_centroid.filter(rep => rep.elected_office === 'MP');
-        let reps = riding.representatives_centroid.filter(rep => rep.elected_office !== 'MP');
-        this.setState({mp: mpList[0], reps });
+        let mlaList = riding.representatives_centroid.filter(rep => rep.elected_office === 'MLA');
+        let reps = riding.representatives_centroid.filter(rep => rep.elected_office !== 'MP' && rep.elected_office !== 'MLA');
+        this.setState({mp: mpList[0], reps, mla: mlaList[0]});
       });
   }
 
@@ -23,8 +24,8 @@ export default class MP extends React.Component {
         <h1>Loading {this.postal}...</h1>
       </>)}
       {this.state.mp && (<>
+        <h2>MP (Federal Rep)</h2>
         <div className="mp-box">
-          <h3>MP</h3>
           <div className="mp-header">
             <img className="mp-image" src={this.state.mp.photo_url} alt={this.state.mp.name} />
             <div className="mp-header-text">
@@ -36,7 +37,20 @@ export default class MP extends React.Component {
           {this.state.mp.extra.twitter && <a href={this.state.mp.extra.twitter} target="_blank" rel="noreferrer noopener"><button className="mp-button mp-button--blue">Twitter</button></a>}
           {this.state.mp.extra.facebook && <a href={this.state.mp.extra.facebook} target="_blank" rel="noreferrer noopener"><button className="mp-button mp-button--darkblue">Facebook</button></a>}
         </div>
-        <h3>Other Reps</h3>
+        <h2>MLA (Provincial Rep)</h2>
+        <div className="mp-box">
+          <div className="mp-header">
+            <img className="mp-image" src={this.state.mla.photo_url} alt={this.state.mla.name} />
+            <div className="mp-header-text">
+              <h1 className="title-reg">{this.state.mla.first_name} <span className="title-bold">{this.state.mla.last_name}</span></h1>
+              <p>{this.state.mla.district_name}</p>
+            </div>
+          </div>
+          <a href={`mailto:${this.state.mla.email}`}><button className="mp-button">Email</button></a>
+          {this.state.mla.extra.twitter && <a href={this.state.mla.extra.twitter} target="_blank" rel="noreferrer noopener"><button className="mp-button mp-button--blue">Twitter</button></a>}
+          {this.state.mla.extra.facebook && <a href={this.state.mla.extra.facebook} target="_blank" rel="noreferrer noopener"><button className="mp-button mp-button--darkblue">Facebook</button></a>}
+        </div>
+        <h2>City Reps</h2>
         {this.state.reps.map(rep => {
           return (
             <div className="mp-box">
