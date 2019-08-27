40b20419dc9be062de8558e5f243a789ca4d4af1
James Madden <jamesbmadden@gmail.com>
2019-08-27 11:10:11 -0700

    Fix to correctly show MP, not candidate (now that election has started and there are candidates running)

--- src/components/MP.js
+++ src/components/MP.js
@@ -6,30 +6,33 @@ export default class MP extends React.Component {
   constructor(props) {
     super(props);
     this.postal = props.postal;
-    this.state = {riding: undefined};
+    this.state = {mp: undefined};
     console.log(`https://represent.opennorth.ca/postcodes/${this.postal}/`);
     fetch(`https://represent.opennorth.ca/postcodes/${this.postal}/`)
       .then(response => response.json())
-      .then(riding => this.setState({riding}));
+      .then(riding => {
+        let mpList = riding.representatives_centroid.filter(rep => rep.elected_office === 'MP');
+        this.setState({mp: mpList[0]});
+      });
   }
 
   render () {
     return (<>
-      {!this.state.riding && (<>
+      {!this.state.mp && (<>
         <h1>Loading {this.postal}...</h1>
       </>)}
-      {this.state.riding && (<>
+      {this.state.mp && (<>
         <div className="mp-box">
           <div className="mp-header">
-            <img className="mp-image" src={this.state.riding.candidates_centroid[0].photo_url} alt={this.state.riding.candidates_centroid[0].name} />
-            <div class="mp-header-text">
-              <h1 className="title-reg">{this.state.riding.candidates_centroid[0].first_name} <span className="title-bold">{this.state.riding.candidates_centroid[0].last_name}</span></h1>
-              <p>{this.state.riding.candidates_centroid[0].district_name}</p>
+            <img className="mp-image" src={this.state.mp.photo_url} alt={this.state.mp.name} />
+            <div className="mp-header-text">
+              <h1 className="title-reg">{this.state.mp.first_name} <span className="title-bold">{this.state.mp.last_name}</span></h1>
+              <p>{this.state.mp.district_name}</p>
             </div>
           </div>
-          <a href={`mailto:${this.state.riding.candidates_centroid[0].email}`}><button className="mp-button">Email</button></a>
-          {this.state.riding.candidates_centroid[0].extra.twitter && <a href={this.state.riding.candidates_centroid[0].extra.twitter} target="_blank" rel="noreferrer noopener"><button className="mp-button mp-button--blue">Twitter</button></a>}
-          {this.state.riding.candidates_centroid[0].extra.facebook && <a href={this.state.riding.candidates_centroid[0].extra.facebook} target="_blank" rel="noreferrer noopener"><button className="mp-button mp-button--darkblue">Facebook</button></a>}
+          <a href={`mailto:${this.state.mp.email}`}><button className="mp-button">Email</button></a>
+          {this.state.mp.extra.twitter && <a href={this.state.mp.extra.twitter} target="_blank" rel="noreferrer noopener"><button className="mp-button mp-button--blue">Twitter</button></a>}
+          {this.state.mp.extra.facebook && <a href={this.state.mp.extra.facebook} target="_blank" rel="noreferrer noopener"><button className="mp-button mp-button--darkblue">Facebook</button></a>}
         </div>
       </>)}
     </>);
